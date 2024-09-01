3d73f72deaf3824250c3eb0d30fd54d6bfe802cb
Сергей <semgek@gmail.com>
2024-09-01 15:24:50 +0300

    Tests for Travelata

---
+++ tests/test_tours/config.py
@@ -0,0 +1,27 @@
+HEADERS = {
+            'accept': 'application/json, text/javascript, */*; q=0.01',
+            'accept-language': 'ru-RU,ru;q=0.9,en-US;q=0.8,en;q=0.7',
+            'origin': 'https://travelata.ru',
+            'referer': 'https://travelata.ru/',
+            'sec-ch-ua': '"Not A;Brand";v="99", "Google Chrome";v="127", "Chromium";v="127"',
+            'sec-ch-ua-mobile': '?0',
+            'sec-ch-ua-platform': '"macOS"',
+            'sec-fetch-dest': 'empty',
+            'sec-fetch-mode': 'cors',
+            'sec-fetch-site': 'same-site',
+            'user-agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/58.0.3029.110 Safari/537.3'
+        }
+
+PARAMS = {
+    'limit': 2000,
+    'departureCity': 2,
+    'country': 92,
+    'checkInDateRange[from]': '2024-09-30',
+    'checkInDateRange[to]': '2024-09-30',
+    'nightRange[from]': 7,
+    'nightRange[to]': 10,
+    'touristGroup[adults]': 2,
+    'touristGroup[kids]': 1,
+    'touristGroup[infants]': 0,
+    'touristGroup[kidsAges][]': 8
+}
